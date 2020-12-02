100b03be86d5cb745e69c9acf1743ade63344d6d
Bastien Jacot-Guillarmod <bjacotg@gmail.com>
2020-12-01 22:46:13 -0500

    Day 1 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 use std::{cmp::Ordering, fs};
 fn main() {
     println!("Day 1: {}", solve_1());
-
 }
 
 fn solve_1() -> String {
-    let contents = fs::read_to_string("input/1.txt")
-        .expect("Unable to read file");
+    let contents = fs::read_to_string("input/1.txt").expect("Unable to read file");
 
-    let mut ints : Vec<_> = contents.lines().map(|int| int.parse::<i32>().expect("Unable to parse integer")).collect();
+    let mut ints: Vec<_> = contents
+        .lines()
+        .map(|int| int.parse::<i32>().expect("Unable to parse integer"))
+        .collect();
     ints.sort();
-    let mut start = ints.iter();
-    let mut end = ints.iter().rev();
-    let mut s = start.next().expect("Empty file");
-    let mut e = end.next().expect("Empty file");
-    loop {
-        match (s + e).cmp(&2020) {
-            Ordering::Equal => return format!("{}", s * e),
-            Ordering::Greater => e = end.next().unwrap(),
-            Ordering::Less => s = start.next().unwrap(),
+    let find_sum = |ints: &[i32], target| {
+        let mut iter = ints.iter();
+        let mut s = iter.next()?;
+        let mut e = iter.next_back()?;
+        loop {
+            match (s + e).cmp(&target) {
+                Ordering::Equal => break Some(s * e),
+                Ordering::Greater => e = iter.next_back()?,
+                Ordering::Less => s = iter.next()?,
+            }
         }
-    }
+    };
+    let part1 = find_sum(&ints, 2020).expect("Could not find values for part 1");
 
+    let part2 = {
+        let mut iter = ints.iter().enumerate();
+        loop {
+            let (idx, value) = iter.next().expect("Could not find solution for part 2");
+            if let Some(pdt) = find_sum(&ints[idx + 1..], 2020 - value) {
+                break pdt * value;
+            }
+        }
+    };
 
+    format!("{:?} {:?}", part1, part2)
 }
